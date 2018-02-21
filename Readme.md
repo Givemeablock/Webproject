d97108da08981534c21b137b1da1021f5b588a4f
Givemeablock <yangyuanchangdali@163.com>
2018-02-21 15:52:55 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+SSM框架项目
+
+参考博客:
+
+[1.idea配置maven](http://blog.csdn.net/mr_ooo/article/details/53871828)
---
+++ src/main/Test.java
@@ -0,0 +1,7 @@
+import javax.servlet.http.HttpServlet;
+
+public class Test extends HttpServlet{
+    public static void main(String[] args) {
+
+    }
+}
