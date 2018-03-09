269bf54b3a4c66d62f8677da37cf364cd5808675
Givemeablock <yangyuanchangdali@163.com>
2018-03-09 20:53:52 +0800

    新增店铺dao和相关测试

---
+++ src/main/java/com/yyc/webpro/dao/ShopDao.java
@@ -0,0 +1,15 @@
+package com.yyc.webpro.dao;
+
+import com.yyc.webpro.entity.Shop;
+
+public interface ShopDao {
+    /**
+     * 新增店铺
+     *
+     * @param shop
+     * @return
+     */
+    int insertShop(Shop shop);
+
+    int updateShop(Shop shop);
+}
---
+++ src/test/java/com/yyc/webpro/dao/ShopDaoTest.java
@@ -0,0 +1,57 @@
+package com.yyc.webpro.dao;
+
+import com.yyc.webpro.BasicTest;
+import com.yyc.webpro.entity.Area;
+import com.yyc.webpro.entity.PersonInfo;
+import com.yyc.webpro.entity.Shop;
+import com.yyc.webpro.entity.ShopCategory;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class ShopDaoTest extends BasicTest {
+
+    @Autowired
+    private ShopDao shopDao;
+
+    @Test
+    //这将不会执行该测试用例
+    @Ignore
+    public void testInsertShop(){
+        Shop shop = new Shop();
+        PersonInfo personInfo = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        personInfo.setUserId(1L);
+        area.setAreaId(2);
+        shopCategory.setShopCategoryId(1L);
+        shop.setOwner(personInfo);
+        shop.setArea(area);
+        shop.setShopCategory(shopCategory);
+        shop.setShopName("test店铺");
+        shop.setShopDesc("test");
+        shop.setShopAddr("test");
+        shop.setPhone("test");
+        shop.setShopImg("test");
+        shop.setCreateTime(new Date());
+        shop.setEnableStatus(1);
+        shop.setAdvice("审核中");
+
+        shopDao.insertShop(shop);
+
+    }
+
+    @Test
+    public void testUpdateShop(){
+        Shop shop = new Shop();
+        shop.setShopId(1L);
+        shop.setShopDesc("test更改");
+        shop.setUpdateTime(new Date());
+
+        int effectNum = shopDao.updateShop(shop);
+        assert 1 == effectNum;
+
+    }
+}
